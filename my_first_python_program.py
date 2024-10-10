305c50c54a94baa9c0d2386dbd7db131c5b51c44
makaelynturner <149030798+makaelynturner@users.noreply.github.com>
2024-10-10 13:59:09 -0400

    Add files via upload

---
+++ my_first_python_program.py
@@ -0,0 +1,9 @@
+# Setting variables for the animals
+c = "cats"  # Variable for "cats"
+d = "dogs"  # Variable for "dogs"
+
+# Creating a sentence using the variables
+s = "It is raining " + c + " and " + d + "."  # The sentence to print
+
+# Display the sentence
+print(s)  # Printing the final output
